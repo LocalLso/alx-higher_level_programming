514c4e516fe297e5b7ba9f39f57ad8941d48ecc5
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-12-01 15:02:52 +0000

    Python scipit that imports the function def add(a, b): from the file add_0.py and prints the result of the addition.

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+        Returns:
+            The return value. a + b
+        """
+    return (a + b)
