618d44a9ce0ac93f52a4e883c31faabd618ebf00
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-12-06 01:59:36 +0000

    Python scripts and C files.

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,5 @@
 def no_c(my_string):
     """ Python function that removes characters """
 
-    for i in my_string:
-        new_string = my_string.translate({ord(i): None for i in 'cC'})
-        return (new_string)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return (new_string)
