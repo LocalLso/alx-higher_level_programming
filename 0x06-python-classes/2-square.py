b340776a807687f3370a25e02abc949fceb49b12
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-12-20 22:35:57 +0000

    Python scripts that creates a class Square that defines a square:Instantiation with optional size, and private instance attribute:size.

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,5 +9,5 @@ class Square:
         self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
