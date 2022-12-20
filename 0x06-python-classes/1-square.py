4f32314c3fdb1397e92d5425f71e64ded2fac9bc
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2022-12-20 22:01:13 +0000

    Python script that creates class Square that defines a square: Private instance attribute: size, Instantiation with size (no type/value verification).

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module defines a square. """
+
+
+class Square:
+    """ A class representing a square. """
+    def __init__(self, size):
+        """ Init of instances. """
+        self.__size = size
