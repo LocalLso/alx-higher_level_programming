4655b7b8a92a5a15d9b989f8c5dd73c5a2b40ff6
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-01-03 09:37:53 +0000

    Python script with a class together with property getter and setter.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,10 +4,11 @@
 
 class Rectangle:
     """ This class represnets a rectangle. """
+
     def __init__(self, width=0, height=0):
         """ This initialize the instances of the class. """
         self.width = width
-        self.hieght = height
+        self.height = height
 
     @property
     def width(self):
@@ -17,9 +18,9 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Width setter. """
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -33,6 +34,6 @@ class Rectangle:
         """ Height setter. """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
