fc98062395f91b6f28e8adeb73b6702581eb5fa7
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-01-03 09:25:53 +0000

    Python script with a class together with property getter and setter.

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" This module defines a rectangle. """
+
+
+class Rectangle:
+    """ This class represnets a rectangle. """
+    def __init__(self, width=0, height=0):
+        """ This initialize the instances of the class. """
+        self.width = width
+        self.hieght = height
+
+    @property
+    def width(self):
+        """ Width getter. """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Width setter. """
+        if not isinstance(value,int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height getter. """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Height setter. """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
