5d3474a29f966441d6f92d3aa7075c68a7570c92
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-01-21 02:02:51 +0000

    Python function that creates an Object from a “JSON file”.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,5 @@ import json
 def load_from_json_file(filename):
     """Create a Python object from a JSON file."""
     with open(filename) as f:
-        return json.load(f)
+        toPython = json.load(f)
+        return toPython
