4424050caaca3d8b589bf9f4ab9dd6b51d17d8ab
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-16 14:11:04 +0000

    Javascript script that prints the first argument passed to it.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+if (typeof process.argv[2] === 'undefined') {
+  console.log('No arguments');
+} else {
+  console.log(process.argv[2]);
+}
