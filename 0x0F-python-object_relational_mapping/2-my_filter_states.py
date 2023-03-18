5370440818df446684d6be3d8cad85d9d941cfce
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-18 07:09:25 +0000

    Python script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 """Filter states by user input."""
 
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == '__main__':
 
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
-
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-            id ASC".format(argv[4]))
-    rows = cur.fetchall()
-
-    for row in rows:
-        if row[1] == argv[4]:
-            print(row)
+    sqlQ = ("SELECT * FROM `states` WHERE \
+            BINARY `name` = '{}'".format(sys.argv[4]))
+    cur.execute(sqlQ)
+    results = cur.fetchall()
+    for result in results:
+        print(result)
