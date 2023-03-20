20d7e6e5e097afc1fd75cf127d326eb89e7bcf18
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-20 00:58:41 +0000

    Python script that lists all City objects from the database hbtn_0e_101_usa.

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# Defines a City model.
-# Inherits from SQLAlchemy Base and links to the MySQL table cities.
+"""Defines a City model."""
+
 
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
