c1deb288a105b8830118e36dca4bf26836b9f4a5
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-20 11:32:10 +0000

    Python file that contains the class definition of a State and an instance Base = declarative_base().

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,5 +13,5 @@ class State(Base):
 
     __tablename__ = "states"
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(sting(128), nullable=False)
