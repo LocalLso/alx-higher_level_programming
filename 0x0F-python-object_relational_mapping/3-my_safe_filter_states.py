7e8f6b0e0d630d2a41baabdc84f5ae51dcf5a2b2
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-20 08:04:37 +0000

    Python script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,5 +6,10 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+    sqlQ = ("SELECT * FROM `states`")
+    c.execute(sqlQ)
+    results = c.fetchall()
+    for result in results:
+        if (result[1] == sys.argv[4]):
+            print(result)
+    
