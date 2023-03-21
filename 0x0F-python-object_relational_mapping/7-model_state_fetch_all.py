57f46945c6be3c4af33710567a6daf1af9e8d61b
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-21 10:18:28 +0000

    Python script that lists all State objects from the database hbtn_0e_6_usa.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for result in session.query(State).order_by(State.id):
+        print("{}: {}".format(result.id, result.name))
