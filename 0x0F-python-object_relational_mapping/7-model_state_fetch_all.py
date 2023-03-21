a296729820f083bf6a8e4de006d5312dcf6a76f3
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-21 09:37:10 +0000

    Python script that lists all State objects from the database hbtn_0e_6_usa.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,15 +2,14 @@
 """This module list all states from a db."""
 
 import sys
-from model_state import Base, State
+from model_state import State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+msqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
+    for result in session.query(State).order_by(State.id):
+        print("{}: {}".format(result.id, result.name))
