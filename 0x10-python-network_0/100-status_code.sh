a1fe6bca17da5f3d5dc168613597c29ac1decd49
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-31 02:11:32 +0000

    Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # Sends a request to a URL, and displays only the status code of the response.
-curl -sLI "$1"
+#!/bin/bash
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
