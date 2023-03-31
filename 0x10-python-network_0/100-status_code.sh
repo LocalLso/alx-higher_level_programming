0f1857d57ce1dc0c2eb00cd55b4e286b8e79d37f
LocalLso <maphalethulocupadadeepmakhele5@gmail.com>
2023-03-31 02:22:50 +0000

    Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}\n" "$1"
+curl -o /dev/null -s -w "%{http_code}\n" "$1"
